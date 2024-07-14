b2642864f9d96c414c456afd3e5fb09c8cba9384
Srinivas Koripella <srkoripe@microsoft.com>
2024-07-14 08:51:15 +0000

    More docs

--- 1_bcc_hello/hello.py
+++ 1_bcc_hello/hello.py
@@ -3,7 +3,15 @@ from bcc import BPF
 
 program = r"""
 int hello(void *ctx) {
-    bpf_trace_printk("Hello World!");
+    u64 pid = bpf_get_current_pid_tgid();
+    pid = pid >> 32;
+    char command[128];
+    bpf_get_current_comm(&command, sizeof(command));
+    if (pid%2 == 0) {
+        bpf_trace_printk("Hello World Even!, pid = %u, comm=%s", pid, command);
+    } else {
+        bpf_trace_printk("Hello World Odd!, pid = %u, comm=%s", pid, command);
+    }
     return 0;
 }
 """
